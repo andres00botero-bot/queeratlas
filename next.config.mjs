bf4fd9ba4cd0f615839b25bf62e2e43d8f0f9ca2
Andres Botero <andres00botero@gmail.com>
2026-04-16 19:42:50 +0200

    Remove app-level host redirect to prevent domain redirect loop

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,7 @@
 import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.queeratlas.app",
-          },
-        ],
-        destination: "https://queeratlas.app/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default withSentryConfig(nextConfig, {
   silent: true,
